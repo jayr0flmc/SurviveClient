4992c939037ad328c9f5f9bb2176639222826e2e
jayr0flmc <jaymc@arcor.de>
2016-05-01 12:24:41 +0200

    Added CBA and ACE. Updated client files back to version 0.0.0.0. Updated mod.cpp.
    These are now the basic sandbox client files!

--- mod.cpp
+++ mod.cpp
@@ -1,10 +1,37 @@
-name		= "Survive";
-author		= "Survive Dev-Team";
-action		= "";
-dir			= "@survive";
-logo		= "mod.paa";
-logoOver	= "mod.paa";
-picture		= "survive_client\textures\icon.paa";
-hidePicture	= 0;
-hideName	= 0;
-overview	= "";
+// This is the name that shows up in the Expansions menu.
+name			= "Survive";
+// This is the description of your addon as shows in the Expansions menu.
+overview		= "Coming soon...";
+// Doesn't seem to do anything at the moment.
+// I just keep it there because it puts another mark on your addon.
+// Pretty sure this used to show up somewhere...
+author			= "Survive Dev-Team";
+// 
+dir				= "@survive";
+
+// This is the URL that the action button directs to. Link to a page that helps users keep up to date. I suggest your BI Forum Thread, which can link to all of your distribution methods from there.
+action			= "http://survive.elbental-productions.de";
+// This is the text that shows up on the extremely useful action button when you have an addon selected in the Expansions menu.
+actionName		= "Website";
+
+// This is the text that pops up when you mouse over your logo in the menu
+tooltip			= "Survive";
+// 
+tooltipOwned	= "Survive [Owned]";
+
+// This is the logo that shows up when the addon is selected on the Expansions menu.
+// This will be a fairly large display of the logo, so I encourage an image approximately 512x512.
+picture			= "mod.paa";
+// 
+overviewPicture	= "mod.paa";
+// This is the logo that shows on the main menu of ArmA 3 along the bottom. This is fairly small so I encourage you to at least downsample your logo so it looks better.
+logo			= "mod.paa";
+// Display next to the item added by the mod
+logoSmall		= "mod.paa";
+// This is the image that shows when you mouse over your logo on the menu. You can add a glow, a shadow, or any number of other effects. It gives it a more polished feel and is a neat effect.
+logoOver		= "mod.paa";
+
+//
+hidePicture		= 0;
+// 
+hideName		= 0;
